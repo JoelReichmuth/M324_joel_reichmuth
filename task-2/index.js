e1b8f33ff2f5e464035fc5ea7a792e4bb5e0cbbc
Joël Reichmuth <joel.reichmuth@lehre.roche.com>
2025-11-21 12:06:56 +0100

    Add task 2

---
+++ task-2/index.js
@@ -0,0 +1,8 @@
+// index.js
+export const sum = (a, b) => {
+  return a + b + 1
+}
+console.log(sum(1, 2))
+console.log(myUndefinedVariable)
+var Camel_Message = "test"
+console.log(Camel_Message)
---
+++ task-2/index.test.js
@@ -0,0 +1,8 @@
+import assert from "assert"
+// Datei: index.test.js
+// TODO: Setzen Sie die korrekten Imports ein
+describe("sum", () => {
+    it("should add 1 + 2 to equal 4", () => {
+        assert.equal(sum(1, 2), 4);
+    });
+});
